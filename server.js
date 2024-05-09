6f1c8ab8fe4dbeb9bff207161d4e28210bf97bbb
Dinesh Gupta <alsocoder@gmail.com>
2024-05-09 14:04:14 +0530

    mongodb shift to online

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const db = require('./db');
+require('dotenv').config();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
+
 app.get('/', function (req, res) {
   res.send('Welcome to our hotels');
 })
@@ -13,6 +16,6 @@ app.use('/person', personRoutes);
 const menuItemRoutes = require('./routes/menuItemRoutes');
 app.use('/menu', menuItemRoutes);
 
-app.listen(3000, ()=>{
+app.listen(PORT, ()=>{
     console.log('server running in port 3000');
 })
